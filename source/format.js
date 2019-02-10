1e941fc5340e70f524b9389cec9824558519bfa6
Ivan <ivan.morozov@list.ru>
2019-02-10 17:01:17 +0300

    Добавил чистую функцию, сократил запись new Array(...).join(" ") на .repeat()

--- source/format.js
+++ source/format.js
@@ -10,7 +10,13 @@
 
 function format(arr, n) {
   let localMax = "";
-    
+
+  const pureAssoc = (key, value, object) => ({
+    ...object,
+    [key]: value
+  });
+
+  let res = [];
   for(let i = 0; i < n; i++) {
     for(let j = i; j < arr.length; j+=n) {
       if (String(arr[j]).length > localMax.length) { 
@@ -18,14 +24,14 @@ function format(arr, n) {
       }
     }
     for(let j = i; j < arr.length; j+=n) {
-      arr[j] = new Array(localMax.length - String(arr[j]).length + 1).join(" ") + arr[j];
+      res[j] = pureAssoc(j, ' '.repeat(localMax.length - String(arr[j]).length) + arr[j], arr)[j];
     }
     localMax = "";
   }
 
   let result = "";
-  for(let i = 0; i < arr.length; i += n) {
-    result += (arr.slice(i, i+n).join(" ") + "\n");
+  for(let i = 0; i < res.length; i += n) {
+    result += (res.slice(i, i+n).join(" ") + "\n");
   }
   return (result.slice(0, result.length-1));
 }
